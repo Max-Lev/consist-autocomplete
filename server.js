dab8d27d975cf3f5a6c9670e40e6f3e518091f2e
Max-Lev <maxlevtov@gmail.com>
2018-03-07 17:25:44 +0200

    express deploy

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 4200;
-app.use(express.static(__dirname + '/dist'));
 
-app.get('*',(req,res)=>{
+app.use(express.static(__dirname + '/dist/'));
+
+app.get('*', (req, res) => {
     res.sendFile(__dirname + '/dist/index.html');
 });
 
 
-app.listen(port, () => { console.log(`http://localhost:${port}`) });
+app.listen(process.env.PORT || 4200, () => { console.log(`http://localhost:${port}`); });
